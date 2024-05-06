5ce7cbe8f54df35fa37d5f9689296f5989de981d
unknown <maxsuel.dev@gmail.com>
2024-05-06 18:50:36 -0300

    vidflow project

--- js/script.js
+++ js/script.js
@@ -12,21 +12,23 @@ inputMode.addEventListener('change', function() {
 const listaVideos = document.querySelector('[data-lista]');
 
 function criarCard(element) {
-    if(element.categoria == '') {
-        throw new Error('Vídeo não tem categoria');
-    }
+    if(element.categoria == '') throw new Error('Vídeo não tem categoria');
+
     const liElement = document.createElement('li');
     liElement.className = 'videos-item';
-    liElement.innerHTML = `
+    liElement.innerHTML = htmlElement(element);
+    return liElement;
+}
+
+function htmlElement(element) {
+    return `
         <iframe src="${element.url}" title="${element.titulo}" frameborder="0" allowfullscreen></iframe>
         <div class="descricao-video">
             <img class="img-canal" src="${element.imagem}" alt="Logo do Canal">
             <h3 class="titulo-video">${element.titulo}</h3>
             <p class="titulo-canal">${element.descricao}</p>
             <p class="categoria" hidden>${element.categoria}</p>
-        </div>
-    `;
-    return liElement;
+        </div> `;
 }
 
 async function getDados() {
